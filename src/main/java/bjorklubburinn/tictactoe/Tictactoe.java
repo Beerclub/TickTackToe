5744c001899858600657ec2d7a5f9cff0e18057d
Bjarni Egill <bjarniegill@gmail.com>
2013-11-22 19:24:12 +0000

    Added /tick view where players will play ticktacktoe

--- src/main/java/bjorklubburinn/tictactoe/Tictactoe.java
+++ src/main/java/bjorklubburinn/tictactoe/Tictactoe.java
@@ -8,7 +8,7 @@ public class Tictactoe
 	// Stores the values of the tictactoe table
 	// Will be displayed in a html table
 	public static String[][] tickTack = new String[3][3];
-	
+
 	public static void main(String[] args)
 	{
 		int count = 0;
@@ -24,7 +24,7 @@ public class Tictactoe
 			}
 		}
 
-		// Default homepage 
+		// Default homepage
 		get(new Route("/") 
 	  	{
 	 	 	@Override
@@ -47,6 +47,41 @@ public class Tictactoe
 
 	      	   return welcome;
 	      	}
+	 	});
+
+		// A view where the  player can play Tictactoe
+	 	get(new Route("/tick") 
+	  	{
+	     	@Override
+	      	public Object handle(Request request, Response response) 
+	      	{
+	      		// A string that will store a html code that will be viewed when "/tick" is requested
+	      		StringBuilder output = new StringBuilder();
+				
+				output.append("<form id='tick-submit' method='POST' action='/tick'>")
+
+						.append("<table border=\"1\"><tr>")
+						.append("<td>" + tickTack[0][0] + "</td>")
+						.append("<td>" + tickTack[0][1] + "</td>")
+						.append("<td>" + tickTack[0][2] + "</td>")
+						.append("</tr>")
+						.append("<tr>")
+						.append("<td>" + tickTack[1][0] + "</td>")
+						.append("<td>" + tickTack[1][1] + "</td>")
+						.append("<td>" + tickTack[1][2] + "</td>")
+						.append("</tr>")
+						.append("<tr>")
+						.append("<td>" + tickTack[2][0] + "</td>")
+						.append("<td>" + tickTack[2][1] + "</td>")
+						.append("<td>" + tickTack[2][2] + "</td>")
+						.append("</tr>")
+						.append("</table>")
+						.append("</form>");
+				
+						.append("<input type=\"submit\" value=\"Choose\" 	form=\"tick-submit\" />");
+
+	       		return output;
+	      	} 
 	 	});
 
 
