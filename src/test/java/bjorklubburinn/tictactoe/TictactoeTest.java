1a8a97678ed346c33a4667779dda131c62b4bd4c
Bjarni Egill <bjarniegill@gmail.com>
2013-11-22 20:05:33 +0000

    Added að unit test form resetTickTackArray function

--- src/test/java/bjorklubburinn/tictactoe/TictactoeTest.java
+++ src/test/java/bjorklubburinn/tictactoe/TictactoeTest.java
@@ -5,5 +5,26 @@ import org.junit.Test;
 
 public class TictactoeTest
 {
+	@Test
+	public void testresetTickTackArray()
+	{
+		int count = 0;
+
+		String[][] testTack = new String[3][3];
+
+		// Sets tickTack array to default settings
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				// Adds a html radio button to every positon of the array.
+				// count adds a different value to each radio button so each button has a different value.
+				testTack[i][j] = "<input type=\"radio\" name=\"boardValues\" value=\"" + count + "\">";
+				count++;
+			}
+		}
+
+		assertEquals(testTack, Tictactoe.resetTickTackArray());
+	}
 
 }
